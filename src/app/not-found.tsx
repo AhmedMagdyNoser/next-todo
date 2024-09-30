353719d7d373b541a697024c0e9070bdeb69a9b5
Ahmed Magdy <ahmed.magdy.1138@gmail.com>
2024-09-30 18:20:32 +0300

    Add 404 Not Found page component
    This commit introduces a new `not-found.tsx` file in the `src/app` directory, which contains a simple React component for rendering a 404 Not Found page. The component includes a centered heading displaying "404" and a paragraph stating "Page not found". The styling is managed using Tailwind CSS utility classes.

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div className="flex flex-col items-center px-4 py-12 gap-4">
+      <h1 className="text-5xl font-bold">404</h1>
+      <p>Page not found</p>
+    </div>
+  );
+}
